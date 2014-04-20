3f5385838a49d53836bfb42c75401ab072e9ce8e
Alexandre Medeiros <alexandre.n.medeiros@gmail.com>
2014-04-19 21:35:20 -0300

    add Move structure and functions; prepare game loop
    All that is needed is to fill the playing functions

--- RobotFighter.hs
+++ RobotFighter.hs
@@ -10,6 +10,10 @@ import Control.Monad
 import Data.List.Split
 import System.IO
 
+--
+-- Data types
+--
+
 type Level  = Int
 type Board  = [[Square]]
 type Size   = (Int,Int)
@@ -35,23 +39,70 @@ data Game = Game { player    :: Player
          }
          deriving (Show)
 
--- main function
+data Move = Move { who :: Player
+                 , originX :: Int
+                 , originY :: Int
+                 , destinationX :: Int
+                 , destinationY :: Int
+         }
+         deriving (Read, Show)
+
+--
+-- Main function
+--
 main :: IO ()
 main = do
         hSetBuffering stdout LineBuffering
-        -- Get Game informations
+        -- Get game informations
         pStr <- getLine
         szStr <- getLine
-        boardStr <- replicateM (fst $ parseSize szStr) getLine
-        print $ Game (parsePlayer pStr) (parseSize szStr) (parseBoard boardStr)
-                where
-                        -- Parse strings to data structures
-                        parsePlayer :: String -> Player
-                        parsePlayer str = whoAmI str
-                        parseSize :: String -> Size
-                        parseSize str = readSize $ words str
-                        parseBoard :: [String] -> Board
-                        parseBoard str = map (map readSquare) $ map (chunksOf 2) str
+        let p = whoAmI pStr
+        let sz = readSize $ words szStr
+        brdStr <- replicateM (fst sz) getLine
+        let brd = map (map readSquare) $ map (chunksOf 2) brdStr
+        let game = Game p sz brd
+        -- Start playing
+        startGame game
+
+-- Starting game move:
+--  if I am A, I play,
+--  if I am B, I enter mainLoop
+startGame :: Game -> IO a
+startGame g
+        | player g /= A = mainLoop g
+        | otherwise = do
+                putStrLn $ prettyMove mv
+                mainLoop ng
+                        where
+                                mv = makeMove g
+                                ng = updateGame mv g
+
+-- Main game loop:
+--  get enemy move >> update board >> make move >> updade board
+mainLoop :: Game -> IO a
+mainLoop g = do
+        mStr <- getLine
+        let currGame = updateGame (readMove mStr) g
+        let mv = makeMove currGame
+        putStrLn $ prettyMove mv
+        let newGame = updateGame mv currGame
+        mainLoop newGame
+
+--
+-- Playing functions
+--
+
+-- Update Game board acording to the specified Move
+updateGame :: Move -> Game -> Game
+updateGame _ g = g
+
+-- Make a move based on the game available.
+makeMove :: Game -> Move
+makeMove g = Move (player g) 0 0 1 0
+
+--
+-- Parsing functions
+--
 
 -- Determine which player I am!
 whoAmI :: String -> Player
@@ -73,3 +124,13 @@ readSquare (x:xs)
         | x == 'R' = P . PowerUp $ read xs
         | any (x==) ['A', 'B'] = R $ Robot (read [x]) (read xs)
         | otherwise = error $ "I can't understand this board square:" ++ x:xs ++ " :-("
+
+-- Pretty print move to a String
+prettyMove :: Move -> String
+prettyMove m = unwords [show $ who m, show $ originX m, show $ originY m, show $ destinationX m, show $ destinationY m]
+
+-- Parse a move from a String
+readMove :: String -> Move
+readMove str = Move (read $ w!!0) (read $ w!!1) (read $ w!!2) (read $ w!!3) (read $ w!!4)
+        where
+                w = words str
